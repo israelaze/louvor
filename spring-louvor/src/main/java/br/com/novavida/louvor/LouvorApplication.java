dc1fd5bf2758b6afb12bb54c9f5452d81a24fa9e
israelaze <israelaze@gmail.com>
2022-04-15 20:01:09 -0300

    Projeto base criado

---
+++ spring-louvor/src/main/java/br/com/novavida/louvor/LouvorApplication.java
@@ -0,0 +1,13 @@
+package br.com.novavida.louvor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouvorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouvorApplication.class, args);
+	}
+
+}
