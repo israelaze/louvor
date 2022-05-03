00ea6f12b085fc80d4df8e3fe614df2d7bfcc9cd
israelaze <israelaze@gmail.com>
2022-05-03 17:34:37 -0300

    Atualizando páginas de cadastro e consulta.Abrindo os links em nova aba do navegador

--- angular-louvor/src/app/louvores/louvores.module.ts
+++ angular-louvor/src/app/louvores/louvores.module.ts
@@ -1,10 +1,7 @@
 //Módulos
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CadastrarLouvoresComponent } from './cadastrar-louvores/cadastrar-louvores.component';
-import { ConsultarLouvoresComponent } from './consultar-louvores/consultar-louvores.component';
 import { LouvoresRoutingModule } from './louvores-routing.module';
-
 
 @NgModule({
   declarations: [
